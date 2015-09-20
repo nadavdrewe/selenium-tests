a4ccf2a2f3d1ab249df282b847b79a7c37f3d6a6
sideswipedesigns <jonathantustain@hotmail.com>
2015-09-20 15:35:41 +0100

    First

---
+++ SeleniumTests/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = @"file:///C:\Users\n\Documents\visual studio 2015\Projects\SeleniumTests\SeleniumTests\TestPage.html";
+
+            var table = driver.FindElement(By.TagName("table"));
+            var innerTable = table.FindElement(By.TagName("table"));
+            var row = innerTable.FindElements(By.TagName("td"))[1];
+            Console.WriteLine(row.Text);
+
+
+
+
+
+        }
+    }
+}
